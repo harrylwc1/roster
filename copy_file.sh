e3d439a76146da36209562f0b93f1deeb8a65000
root <root@BE6500.lan>
2026-09-11 09:38:34 +0800

    deleted:    copy_file.sh

--- copy_file.sh
+++
@@ -1,7 +0,0 @@
-for var in "$@"
-do
-    x=$x" -F files=@$var"
-done
-#curl -X POST http://127.0.0.1:8000/upload -F 'files=@multiple-example-1.txt' -F 'files=@multiple-example-2.txt'
-# curl -X POST http://127.0.0.1:8000/upload -F 'files=@basicauth-example.txt' -u hello:world
-curl -X POST http://r619ac2.ubddns.org:8080/upload $x -u $PASS
